5387fa8ccd0486c1f05f72a7ed86a9efd2bc4021
Astone Tuhame <astonetuhame@gmail.com>
2022-01-03 22:58:35 +0300

    Create TaskFactory.php

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'project_id' => Project::factory(),
+            'name' => $this->faker->text,
+        ];
+    }
+}
