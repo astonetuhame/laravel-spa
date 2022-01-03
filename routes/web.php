55dd2526e9c7082ab3958f2d88ff7574a7af8655
Astone Tuhame <astonetuhame@gmail.com>
2022-01-04 01:14:22 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+
+Route::get('/{vue_capture?}', function () {
+    return view('app');
+})->where('vue_capture', '[\/\w\.-]*');
