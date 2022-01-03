16ae832ba0b6da9405e82c1ef6a988caeac153db
Astone Tuhame <astonetuhame@gmail.com>
2022-01-03 23:01:56 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\FilesController;
+use App\Http\Controllers\Api\TasksController;
 use App\Http\Controllers\Api\ProjectController;
 
 /*
@@ -20,6 +22,8 @@ Route::post('/authenticate', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('tasks', TasksController::class)->except(['index', 'show']);
+    Route::post('upload', [FilesController::class, 'store']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
