e1cdadd4dc15a0d68cd4b9ecb9acd579ca2bcdc6
Astone Tuhame <astonetuhame@gmail.com>
2022-01-03 22:59:52 +0300

    Create TasksController.php

---
+++ app/Http/Controllers/Api/TasksController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TaskRequest $request)
+    {
+
+        $this->authorize('create', [Task::class, $request->project_id]);
+
+        $task = Task::create($request->all());
+        return new TaskResource($task);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(TaskRequest $request, Task $task)
+    {
+        $this->authorize('update', $task);
+        $task->update($request->all());
+        return new TaskResource($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        $this->authorize('delete', $task);
+        $task->delete();
+        return ['status' => 'OK'];
+    }
+}
