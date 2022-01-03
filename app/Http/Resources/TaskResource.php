dc07854eb1f2de5de7c29cf8c08aacbb6534da05
Astone Tuhame <astonetuhame@gmail.com>
2022-01-03 22:59:22 +0300

    Create TaskResource.php

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'due_date' => $this->due_date,
+            'priority' => $this->priority,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+
+    public function with($request)
+    {
+        return [
+            'status' => 'OK'
+        ];
+    }
+}
