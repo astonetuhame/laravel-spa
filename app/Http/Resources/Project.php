7198b91a557cf8fe0cf253f0a0d86781fa9106fe
Astone Tuhame <astonetuhame@gmail.com>
2022-01-03 23:01:30 +0300

    Update Project.php

--- app/Http/Resources/Project.php
+++ app/Http/Resources/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Http\Resources\TaskResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Project extends JsonResource
@@ -17,6 +18,9 @@ class Project extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'image' => $this->image_path,
+            'tasks' => TaskResource::collection($this->whenLoaded('tasks')),
+            'tasks_count' => $this->when(!is_null($this->tasks_count), $this->tasks_count),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
