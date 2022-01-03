5a2af7a68d8ef7828e2b6424f0f01cb23914d9dd
Astone Tuhame <astonetuhame@gmail.com>
2022-01-04 02:22:27 +0300

    Create App.vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col min-h-screen justify-between">
+        <Header></Header>
+        <main class="flex flex-grow bg-blue-100">
+            <router-view></router-view>
+        </main>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import Header from "./partials/Header";
+import Footer from "./partials/Footer";
+
+export default {
+    components: {
+        Header,
+        Footer,
+    },
+};
+</script>
