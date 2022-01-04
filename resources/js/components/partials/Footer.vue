bbb2c0ed03cae59074b6f948d88274aebd703b30
Astone Tuhame <astonetuhame@gmail.com>
2022-01-04 13:50:31 +0300

    Update Footer.vue

--- resources/js/components/partials/Footer.vue
+++ resources/js/components/partials/Footer.vue
@@ -1,3 +1,14 @@
 <template>
-    <header class="min-h-10 bg-red-600">Footer</header>
+    <footer
+        class="
+            min-h-10
+            bg-red-600
+            text-red-200
+            flex
+            items-center
+            justify-center
+        "
+    >
+        &copy; 2022 All Rights Reserved
+    </footer>
 </template>
