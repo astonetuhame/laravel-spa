c113a336f60108a870e577d4b8e0249183e8b10d
Astone Tuhame <astonetuhame@gmail.com>
2022-01-04 13:50:36 +0300

    Update Header.vue

--- resources/js/components/partials/Header.vue
+++ resources/js/components/partials/Header.vue
@@ -1,4 +1,35 @@
 <template>
-    <header class="min-h-10 bg-blue-600">Header</header>
-    <h1 class="text-2xl">ssssssssssssss</h1>
+    <header class="min-h-10 bg-blue-800 flex justify-between items-center">
+        <h1 class="text-4xl ml-6 text-white">Projectify</h1>
+        <ul class="flex">
+            <li class="mr-6">
+                <router-link
+                    :to="{ name: 'dashboard' }"
+                    class="text-blue-600 hover:text-white"
+                    >Dashboard</router-link
+                >
+            </li>
+            <li class="mr-6">
+                <router-link
+                    :to="{ name: 'projects' }"
+                    class="text-blue-600 hover:text-white"
+                    >Projects</router-link
+                >
+            </li>
+            <li class="mr-6">
+                <router-link
+                    :to="{ name: 'login' }"
+                    class="text-blue-600 hover:text-white"
+                    >Login</router-link
+                >
+            </li>
+            <li class="mr-6">
+                <router-link
+                    :to="{ name: 'dashboard' }"
+                    class="text-blue-600 hover:text-white"
+                    >Logout</router-link
+                >
+            </li>
+        </ul>
+    </header>
 </template>
