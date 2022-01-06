b70da2701d69bc3f2a21ddd5c244be481854b491
Astone Tuhame <astonetuhame@gmail.com>
2022-01-07 01:32:29 +0300

    Update Projects.vue

--- resources/js/components/projects/Projects.vue
+++ resources/js/components/projects/Projects.vue
@@ -3,10 +3,18 @@
         <div class="flex justify-between">
             <h1 class="text-2xl text-gray-700">Projects</h1>
             <button
+                @click="showForm = true"
                 class="bg-red-500 rounded text-white px-3 py-2 mr-6 hover:bg-gray-700"
             >
                 Add Project
             </button>
+        </div>
+
+        <div v-show="showForm" class="flex justify-center">
+            <ProjectForm
+                @project-added="fetchProjects"
+                @cancel-form="showForm = false"
+            />
         </div>
         <div class="justify-center flex">
             <table class="table-auto justify-center">
@@ -20,6 +28,7 @@
                 </thead>
                 <tbody>
                     <ProjectItem
+                        @project-deleted="fetchProjects"
                         v-for="project in projects"
                         :key="project.id"
                         :project="project"
@@ -32,19 +41,28 @@
 
 <script>
 import ProjectItem from "./ProjectItem.vue";
+import ProjectForm from "./ProjectForm.vue";
 export default {
     components: {
         ProjectItem,
+        ProjectForm,
     },
     data() {
         return {
             projects: [],
+            showForm: false,
         };
     },
+    methods: {
+        fetchProjects() {
+            this.showForm = false;
+            axios.get("api/projects").then((res) => {
+                this.projects = res.data.data;
+            });
+        },
+    },
     mounted() {
-        axios.get("api/projects").then((res) => {
-            this.projects = res.data.data;
-        });
+        this.fetchProjects();
     },
 };
 </script>
