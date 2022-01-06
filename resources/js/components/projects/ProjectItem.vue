8f60641b44dea84e51aba403c584620bc5aeb241
Astone Tuhame <astonetuhame@gmail.com>
2022-01-06 17:45:04 +0300

    Create ProjectItem.vue

---
+++ resources/js/components/projects/ProjectItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <tr>
+        <td class="border px-4 py-2">{{ project.id }}</td>
+        <td class="border px-4 py-2">{{ project.name }}</td>
+        <td class="border px-4 py-2">
+            {{ project.tasks_count }}
+        </td>
+        <td class="border px-4 py-2">
+            <button
+                class="bg-gray-500 rounded text-white px-3 py-2 mr-2 hover:bg-gray-700"
+            >
+                View
+            </button>
+            <button
+                class="bg-red-500 rounded text-white px-3 py-2 mr-2 hover:bg-gray-700"
+            >
+                Delete
+            </button>
+        </td>
+    </tr>
+</template>
+
+<script>
+export default {
+    props: ["project"],
+};
+</script>
