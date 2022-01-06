4beb7bad56241e9cfcf87535d571849f37cd542f
Astone Tuhame <astonetuhame@gmail.com>
2022-01-06 16:19:58 +0300

    Update routes.js

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,14 +2,37 @@ import { createRouter, createWebHistory } from "vue-router";
 import Signin from "./components/auth/Signin.vue";
 import Dashboard from "./components/dashboard/Dashboard.vue";
 import Project from "./components/projects/Project.vue";
+import store from "./store/index";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/login", component: Signin, name: "login" },
-        { path: "/dashboard", component: Dashboard, name: "dashboard" },
-        { path: "/projects", component: Project, name: "projects" },
+        { path: "/", component: Signin, name: "home" },
+        { path: "/login", component: Signin, name: "login", alias: "/" },
+        {
+            path: "/dashboard",
+            component: Dashboard,
+            name: "dashboard",
+            meta: { requiresAuth: true },
+        },
+        {
+            path: "/projects",
+            component: Project,
+            name: "projects",
+            meta: { requiresAuth: true },
+        },
     ],
 });
 
+router.beforeEach((to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (!store.getters.authenticated) {
+            next({ name: "login" });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
 export default router;
