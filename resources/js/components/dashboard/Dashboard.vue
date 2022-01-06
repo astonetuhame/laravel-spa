80d286deb30a97f7752e59257b16e0865bab1fee
Astone Tuhame <astonetuhame@gmail.com>
2022-01-06 16:33:05 +0300

    Update Dashboard.vue

--- resources/js/components/dashboard/Dashboard.vue
+++ resources/js/components/dashboard/Dashboard.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        {{ user }}
+    <div class="p-4 m-4 bg-white rounded flex">
+        <div>
+            <h1 class="text-2xl text-gray-700">Dashboard</h1>
+            <p class="mt-4">
+                Hello <span class="font-bold">{{ user.name }}</span> Welcome to
+                the Dashboard. You have registered with the email
+                <span class="italic"> {{ user.email }}</span>
+            </p>
+            <p class="mt-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea et
+                dolorem incidunt pariatur repudiandae impedit alias. Quos
+                recusandae a consequatur iste natus harum sapiente corporis
+                eligendi, dicta rerum veritatis sunt?
+            </p>
+            <p class="mt-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea et
+                dolorem incidunt pariatur repudiandae impedit alias. Quos
+                recusandae a consequatur iste natus harum sapiente corporis
+                eligendi, dicta rerum veritatis sunt?
+            </p>
+        </div>
     </div>
 </template>
 
