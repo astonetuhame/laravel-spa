ac1e4beb1e2208d57b553f3512f4d97ad79a1f27
Astone Tuhame <astonetuhame@gmail.com>
2022-01-06 16:19:34 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,13 @@ require("./bootstrap");
 import { createApp } from "vue";
 import router from "./routes";
 import App from "./components/App";
+import axios from "axios";
+import store from "./store/index";
 
-const app = createApp(App);
-app.use(router).mount("#app");
+axios.defaults.withCredentials = true;
+//axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+
+store.dispatch("getUser").then(() => {
+    const app = createApp(App);
+    app.use(router).use(store).mount("#app");
+});
