fa86582e8b0371679dc6c4f055025050802f1496
Astone Tuhame <astonetuhame@gmail.com>
2022-01-07 13:08:12 +0300

    Create TaskItem.vue

---
+++ resources/js/components/projects/TaskItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        class="mb-2 bg-gray-300 grid grid-cols-3"
+        :class="{ 'bg-gray-600': task.priority == 'High' }"
+    >
+        <div class="flex items-center">
+            {{ task.name }}
+        </div>
+        <div class="flex items-center">
+            {{ task.due_date }}
+        </div>
+        <div class="flex items-center">
+            <button
+                class="bg-orange-500 rounded text-white px-3 py-2 mr-2 hover:bg-gray-700"
+            >
+                Edit
+            </button>
+            <button
+                class="bg-red-500 rounded text-white px-3 py-2 mr-2 hover:bg-gray-700"
+            >
+                Delete
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["task"],
+};
+</script>
